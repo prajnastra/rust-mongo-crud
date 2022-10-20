59e2540a8ae88aa0e28e4d47b5780edf27afbb91
ethabhijit <coder.abhijit90@gmail.com>
2022-10-20 08:57:37 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+
+use api::user_api::{create_user, get_user};
+use repository::mongodb_repo::MongoRepo;
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
+}
