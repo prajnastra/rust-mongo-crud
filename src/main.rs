14c57a1d064fed77d70c749dccd2e3e8211b0d1c
ethabhijit <coder.abhijit90@gmail.com>
2022-10-22 08:31:39 +0530

    add: update, delete, get all route

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod repository;
 #[macro_use]
 extern crate rocket;
 
-use api::user_api::{create_user, get_user};
+use api::user_api::{create_user, delete_user, get_all_users, get_user, update_user};
 use repository::mongodb_repo::MongoRepo;
 
 #[launch]
@@ -15,4 +15,7 @@ fn rocket() -> _ {
         .manage(db)
         .mount("/", routes![create_user])
         .mount("/", routes![get_user])
+        .mount("/", routes![update_user])
+        .mount("/", routes![delete_user])
+        .mount("/", routes![get_all_users])
 }
--- src/repository/mongodb_repo.rs
+++ src/repository/mongodb_repo.rs
@@ -5,7 +5,7 @@ use dotenv::dotenv;
 use crate::models::user_model::User;
 use mongodb::{
     bson::{doc, extjson::de::Error, oid::ObjectId},
-    results::InsertOneResult,
+    results::{DeleteResult, InsertOneResult, UpdateResult},
     sync::{Client, Collection},
 };
 
@@ -52,5 +52,46 @@ impl MongoRepo {
             .ok()
             .expect("Error getting user's details");
         Ok(user_detail.unwrap())
+    }
+
+    pub fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! { "_id": obj_id };
+        let new_doc = doc! {
+            "$set": {
+                "id": new_user.id,
+                "name": new_user.name,
+                "location": new_user.location,
+                "title": new_user.title
+            }
+        };
+
+        let update_doc = self
+            .col
+            .update_one(filter, new_doc, None)
+            .ok()
+            .expect("Error updating user");
+        Ok(update_doc)
+    }
+
+    pub fn delete_user(&self, id: &String) -> Result<DeleteResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let user_detail = self
+            .col
+            .delete_one(filter, None)
+            .ok()
+            .expect("Error deleting user");
+        Ok(user_detail)
+    }
+
+    pub fn get_all_users(&self) -> Result<Vec<User>, Error> {
+        let cursors = self
+            .col
+            .find(None, None)
+            .ok()
+            .expect("Error getting list of users");
+        let users = cursors.map(|doc| doc.unwrap()).collect();
+        Ok(users)
     }
 }
